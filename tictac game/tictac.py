f6fb0f752f86841f1b3a746f891478c2df4f83ff
Ben-Aubrey <109590243+Ben-Aubrey@users.noreply.github.com>
2023-06-01 08:48:59 +0000

    Refactor tictac.py

--- tictac game/tictac.py
+++ tictac game/tictac.py
@@ -5,126 +5,94 @@ from tkinter import *
 import random
 
 def next_turn(row, column):
-
     global player
 
     if buttons[row][column]['text'] == "" and check_winner() is False:
+        buttons[row][column]['text'] = player
 
-        if player == players[0]:
-
-            buttons[row][column]['text'] = player
-
-            if check_winner() is False:
-                player = players[1]
-                label.config(text=(players[1]+" turn"))
-
-            elif check_winner() is True:
-                label.config(text=(players[0]+" wins"))
-
-            elif check_winner() == "Tie":
-                label.config(text="Tie!")
-
+        if check_winner():
+            label.config(text=player + " wins")
+        elif check_tie():
+            label.config(text="Tie!")
         else:
-
-            buttons[row][column]['text'] = player
-
-            if check_winner() is False:
-                player = players[0]
-                label.config(text=(players[0]+" turn"))
-
-            elif check_winner() is True:
-                label.config(text=(players[1]+" wins"))
-
-            elif check_winner() == "Tie":
-                label.config(text="Tie!")
+            player = players[1] if player == players[0] else players[0]
+            label.config(text=player + " turn")
 
 def check_winner():
-
-    for row in range(3):
-        if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
-            buttons[row][0].config(bg="green")
-            buttons[row][1].config(bg="green")
-            buttons[row][2].config(bg="green")
+    for i in range(3):
+        if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] != "":
+            buttons[i][0].config(bg="lime green")
+            buttons[i][1].config(bg="lime green")
+            buttons[i][2].config(bg="lime green")
             return True
 
-    for column in range(3):
-        if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
-            buttons[0][column].config(bg="green")
-            buttons[1][column].config(bg="green")
-            buttons[2][column].config(bg="green")
+    for i in range(3):
+        if buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] != "":
+            buttons[0][i].config(bg="lime green")
+            buttons[1][i].config(bg="lime green")
+            buttons[2][i].config(bg="lime green")
             return True
 
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
-        buttons[0][0].config(bg="green")
-        buttons[1][1].config(bg="green")
-        buttons[2][2].config(bg="green")
+        buttons[0][0].config(bg="lime green")
+        buttons[1][1].config(bg="lime green")
+        buttons[2][2].config(bg="lime green")
         return True
 
     elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
-        buttons[0][2].config(bg="green")
-        buttons[1][1].config(bg="green")
-        buttons[2][0].config(bg="green")
+        buttons[0][2].config(bg="lime green")
+        buttons[1][1].config(bg="lime green")
+        buttons[2][0].config(bg="lime green")
         return True
 
-    elif empty_spaces() is False:
+    return False
 
-        for row in range(3):
-            for column in range(3):
-                buttons[row][column].config(bg="yellow")
-        return "Tie"
-
-    else:
-        return False
-
-
-def empty_spaces():
-
-    spaces = 9
-
-    for row in range(3):
-        for column in range(3):
-            if buttons[row][column]['text'] != "":
-                spaces -= 1
-
-    if spaces == 0:
-        return False
-    else:
-        return True
+def check_tie():
+    for row in buttons:
+        for button in row:
+            if button['text'] == "":
+                return False
+    return True
 
 def new_game():
-
     global player
 
     player = random.choice(players)
+    label.config(text=player + " turn")
 
-    label.config(text=player+" turn")
-
-    for row in range(3):
-        for column in range(3):
-            buttons[row][column].config(text="",bg="#F0F0F0")
-
+    for row in buttons:
+        for button in row:
+            button.config(text="", bg="#F0F0F0")
 
 window = Tk()
 window.title("Tic-Tac-Toe")
-players = ["x","o"]
+players = ["X", "O"]
 player = random.choice(players)
-buttons = [[0,0,0],
-           [0,0,0],
-           [0,0,0]]
 
-label = Label(text=player + " turn", font=('consolas',40))
-label.pack(side="top")
+buttons = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-reset_button = Button(text="restart", font=('consolas',20), command=new_game)
-reset_button.pack(side="top")
+label = Label(text=player + " turn", font=("Arial", 24), fg="#333333")
+label.pack(pady=10)
 
-frame = Frame(window)
-frame.pack()
+reset_button = Button(text="Restart", font=("Arial", 16), command=new_game, bg="#CCCCCC")
+reset_button.pack(pady=10)
 
-for row in range(3):
-    for column in range(3):
-        buttons[row][column] = Button(frame, text="",font=('consolas',40), width=5, height=2,
-                                      command= lambda row=row, column=column: next_turn(row,column))
-        buttons[row][column].grid(row=row,column=column)
+frame = Frame(window, bg="#CCCCCC")
+frame.pack(pady=20)
+
+for i in range(3):
+    for j in range(3):
+        buttons[i][j] = Button(frame, text="", font=("Arial", 36), width=5, height=2,
+                              command=lambda row=i, col=j: next_turn(row, col), bg="#EEEEEE")
+        buttons[i][j].grid(row=i, column=j, padx=5, pady=5)
+
+window.update_idletasks()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+x_coordinate = int((screen_width / 2) - (window_width / 2))
+y_coordinate = int((screen_height / 2) - (window_height / 2))
+window.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
 
 window.mainloop()
